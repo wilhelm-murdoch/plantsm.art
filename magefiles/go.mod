6e123fe39551aa18888d236b2c9957664937ed37
Wilhelm Murdoch <wilhelm@devilmayco.de>
2022-12-02 12:48:34 +1000

    bootstrapping primary data source

---
+++ magefiles/go.mod
@@ -0,0 +1,16 @@
+module github.com/wilhelm-murdoch/plantsm.art
+
+go 1.19
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/gosimple/slug v1.13.1
+	github.com/wilhelm-murdoch/go-collection v1.0.9
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/gosimple/unidecode v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -1,22 +1,25 @@
-//go:build mage
-// +build mage
-
 package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"regexp"
 	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/gosimple/slug"
+	"github.com/wilhelm-murdoch/go-collection"
 )
 
 const (
-	baseUrl = "https://www.aspca.org/pet-care/animal-poison-control/%s-plant-list"
+	baseUrlRoot = "https://www.aspca.org"
+	baseUrl     = "https://www.aspca.org/pet-care/animal-poison-control/%s-plant-list"
 )
 
 func getDocumentFromUrl(url string) (*goquery.Document, error) {
@@ -56,44 +59,122 @@ func getDocumentFromUrl(url string) (*goquery.Document, error) {
 	return nil, errors.New("this should not happen, but here we are")
 }
 
+type Classification struct {
+	Kingdom string   `json:"kingdom"`
+	Clades  []string `json:"clades"`
+	Order   string   `json:"order"`
+	Family  string   `json:"family"`
+	Genus   string   `json:"genus"`
+	Species string   `json:"species"`
+}
+
 type Plant struct {
-	Id             string
-	Name           string
-	Common         []string
-	Images         []string
-	Classification struct {
-		Kingdom string
-		Clade   string
-		Order   string
-		Family  string
-		Genus   string
-		Species string
-	}
+	Id             string         `json:"id"`
+	Name           string         `json:"name"`
+	ScientificName string         `json:"scientific_name"`
+	Common         []string       `json:"common"`
+	Images         []string       `json:"images"`
+	Symptoms       []string       `json:"symptoms"`
+	SourceUrl      string         `json:"source_url"`
+	Classification Classification `json:"classification"`
 }
 
 type Data struct {
-	PlantsByCat []string
-	PlantsByDog []string
-	Plants      []Plant
+	PlantsByCat      *collection.Collection[string] `json:"plants_by_cat"`
+	PlantsByDog      *collection.Collection[string] `json:"plants_by_dog"`
+	PlantsBySymptoms [][]string                     `json:"plants_by_symptoms"`
+	Plants           *collection.Collection[*Plant] `json:"plants"`
 }
 
-func ImportCats() error {
-	document, err := getDocumentFromUrl(fmt.Sprintf(baseUrl, "cats"))
+func (d *Data) NewPlantForAnimal(animal string, s *goquery.Selection) *Plant {
+	source_url, _ := s.Find("a").First().Attr("href")
+
+	pattern := regexp.MustCompile(`^(.+?)\s\((.*?)\).+Scientific\sNames:\s?(.+?)?\s\|\sFamily:(.+?)?$`)
+	matches := pattern.FindStringSubmatch(s.Text())
+
+	var common_names []string
+	for _, name := range strings.Split(matches[2], ",") {
+		if name != "" {
+			common_names = append(common_names, strings.TrimSpace(name))
+		}
+	}
+
+	return &Plant{
+		Id:             slug.Make(matches[1]),
+		Name:           strings.TrimSpace(matches[1]),
+		ScientificName: strings.TrimSpace(matches[3]),
+		Common:         common_names,
+		SourceUrl:      fmt.Sprintf("%s%s", baseUrlRoot, source_url),
+		Classification: Classification{
+			Family: strings.TrimSpace(matches[4]),
+		},
+	}
+}
+
+func Import(ctx context.Context) error {
+	data := &Data{
+		PlantsByCat: collection.New[string](),
+		PlantsByDog: collection.New[string](),
+		Plants:      collection.New[*Plant](),
+	}
+
+	if err := importPlantsForAnimal("cats", data); err != nil {
+		return err
+	}
+
+	if err := importPlantsForAnimal("dogs", data); err != nil {
+		return err
+	}
+
+	_, err := data.Plants.Batch(func(b, j int, plant *Plant) (*Plant, error) {
+		fmt.Println("Processing ...", plant.Id)
+
+		document, err := getDocumentFromUrl(plant.SourceUrl)
+		if err != nil {
+			return nil, err
+		}
+
+		symptoms := document.Find("div.field-name-field-clinical-signs span.values").Text()
+
+		plant.Symptoms = strings.Split(symptoms, ",")
+
+		return nil, nil
+	}, 5)
+
 	if err != nil {
 		return err
 	}
 
-	document.Find("div.views-row span.field-content").Each(func(i int, s *goquery.Selection) {
-		fmt.Print(strings.TrimSpace(s.Text()))
-	})
+	encoder := json.NewEncoder(os.Stdout)
+	if err := encoder.Encode(data); err != nil {
+		return err
+	}
 
 	return nil
 }
 
-func Import(ctx context.Context, animal string) error {
+func importPlantsForAnimal(animal string, data *Data) error {
 	if animal != "dogs" && animal != "cats" {
 		return fmt.Errorf("expected \"cats\" or \"dogs\", but got \"%s\" instead", animal)
 	}
 
-	return ImportCats()
+	document, err := getDocumentFromUrl(fmt.Sprintf(baseUrl, animal))
+	if err != nil {
+		return err
+	}
+
+	document.Find("div.view-content").First().Find("div.views-row span.field-content").Each(func(i int, s *goquery.Selection) {
+		plant := data.NewPlantForAnimal(animal, s)
+
+		switch {
+		case animal == "cats":
+			data.PlantsByCat.PushDistinct(plant.Id)
+		case animal == "dogs":
+			data.PlantsByDog.PushDistinct(plant.Id)
+		}
+
+		data.Plants.PushDistinct(plant)
+	})
+
+	return nil
 }
