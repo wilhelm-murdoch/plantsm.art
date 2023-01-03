0df6493127bdd3b78b2cd3f619ccc5fd275dc6ed
Wilhelm Murdoch <wilhelm@devilmayco.de>
2023-01-03 12:12:08 +1000

    updating static pages

--- src/routes/contribute/+page.svelte
+++ src/routes/contribute/+page.svelte
@@ -1,1 +1,15 @@
-TBD
+<div class="relative bg-gray-50 mb-8 sm:px-6 sm:pt-4 lg:px-8 lg:pt-4 lg:pb-4 border-y">
+	<div class="relative mx-auto max-w-7xl">
+		<div class="overflow-hidden rounded-lg shadow-lg mb-8 bg-white p-10">
+			<div class="mx-auto max-w-prose text-lg">
+				<h1>
+					<span class="block text-center text-lg font-semibold text-green-600">Wanna Help?</span>
+					<span
+						class="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl"
+						>Learn How to Contribute!</span
+					>
+				</h1>
+			</div>
+		</div>
+	</div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,6 @@ module.exports = {
 	},
 	plugins: [
 		require('@tailwindcss/forms'),
-		require('flowbite/plugin')
+		require('@tailwindcss/typography')
 	]
 };
