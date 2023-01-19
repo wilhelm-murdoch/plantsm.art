61ebed240a22b64b80eb102503bb43fac76f5741
Wilhelm Murdoch <wilhelm@devilmayco.de>
2023-01-19 23:56:51 +1000

    updated param use

--- src/routes/plant/[slug]/+page.ts
+++ src/routes/plant/[slug]/+page.ts
@@ -1,9 +1,8 @@
 export const prerender = true;
 
 // src/routes/blog/[slug]/+page.ts
-export async function load(params: any) {
-  console.log(params)
-  const plant = await import(`../../../lib/data/plants/${params.params.slug}.json`)
+export async function load({params}) {
+  const plant = await import(`../../../lib/data/plants/${params.slug}.json`)
   return {
     plant: plant
   }
