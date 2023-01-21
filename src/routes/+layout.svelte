5a7ca88589f5f6f7e61d2b440ee195990c2f254d
Wilhelm Murdoch <wilhelm@devilmayco.de>
2023-01-21 14:52:00 +1000

    add responsive top menu for smaller screens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,6 @@
 	import { Header, Footer } from '$components';
 </script>
 
-<div class="hidden text-yellow-700 bg-yellow-100 text-center py-2.5 text-sm border border-b-yellow-300">
-	<p>Hi! 👋 😬 This project is still under heavy development and heaps of features are still being worked on and finessed.</p>
-</div>
-
 <Header />
 
 <slot />
