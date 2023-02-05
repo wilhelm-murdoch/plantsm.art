596d73d9604ac6aa11a8df1823fa4fae5e50426a
Wilhelm Murdoch <wilhelm@devilmayco.de>
2023-02-06 02:00:33 +1000

    more styling updates

--- src/lib/components/Details/ColumnRight.svelte
+++ src/lib/components/Details/ColumnRight.svelte
@@ -87,7 +87,7 @@
 		<h3 class="unstyled font-sans p-2.5 border-b text-md font-semibold bg-slate-100 text-slate-500">Images</h3>
 		<div class="grid grid-cols-4">
 			{#each plant.images as image, i (image)}
-				<div class="overflow-hidden background-fallback bg-slate-50">
+				<div class="overflow-hidden background-fallback bg-green-50">
 					<img on:click={() => openLightbox(image, i)} on:keydown alt={image.relative_path} class="is-lazy  inline-block object-cover object-center h-32 w-full hover:scale-110 ease-in-out duration-100 cursor-pointer" use:lazy={getImageUrl(image.relative_path, 'thumbnail')} />
 				</div>
 			{/each}
